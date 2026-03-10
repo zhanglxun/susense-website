93bb736eea8898818ecd3605f8447d46faa9ea4e
jarvis <zhanglxun@qq.com>
2026-03-10 19:30:57 +0800

    更新部分菜单

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -13,40 +13,99 @@
           <i class="fe fe-x"></i>
         </button>
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="index.html">首页</a>
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">首页</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarPages" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">方案与服务</a>
-            
-            <div class="dropdown-menu dropdown-menu-lg" aria-labelledby="navbarPages">
+            <a class="nav-link dropdown-toggle" id="navbarServices" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">服务与方案</a>
+            <div class="dropdown-menu dropdown-menu-lg" aria-labelledby="navbarServices" style="min-width: 580px;">
               <div class="row no-gutters">
                 <div class="col-6 col-lg-6">
-                  <h5 class="dropdown-header mb-5">解决方案</h5>
-                  <a class="dropdown-item mb-3" href="case-business.html">健康管理</a>
-                  <a class="dropdown-item mb-3" href="case-data.html">数字疗法</a>
-                  <a class="dropdown-item mb-3" href="case-ai.html">数据解决方案</a>
-                  <a class="dropdown-item mb-3" href="case-ai.html">智慧零售</a>
+                  <h6 class="dropdown-header text-uppercase text-primary">服务</h6>
+                  <div class="list-group list-group-flush">
+                    <a class="list-group-item" href="#">
+                      <div class="icon icon-sm text-primary">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.325 3.05L8.667 20.432l1.932.518 4.658-17.382-1.932-.518zM7.612 18.36l1.36-1.448-4.16-3.912 4.16-3.91-1.36-1.45-5.5 5.36 5.5 5.36zM16.388 18.36l5.5-5.36-5.5-5.36-1.36 1.45 4.16 3.91-4.16 3.912 1.36 1.448z" fill="#335EEA"/>
+                        </svg>
+                      </div>
+                      <div class="ml-4">
+                        <h6 class="font-weight-bold text-uppercase text-primary mb-0">软件系统开发</h6>
+                        <p class="font-size-sm text-gray-700 mb-0">企业级系统定制开发</p>
+                      </div>
+                    </a>
+                    <a class="list-group-item" href="#">
+                      <div class="icon icon-sm text-primary">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" fill="#335EEA" opacity="0.3"/>
+                          <path d="M17 3H7c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H7V5h10v14z" fill="#335EEA"/>
+                        </svg>
+                      </div>
+                      <div class="ml-4">
+                        <h6 class="font-weight-bold text-uppercase text-primary mb-0">产品体验设计</h6>
+                        <p class="font-size-sm text-gray-700 mb-0">APP与产品体验优化</p>
+                      </div>
+                    </a>
+                    <a class="list-group-item" href="#">
+                      <div class="icon icon-sm text-primary">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M21 10.12h-6.78l2.74-2.82c-2.73-2.7-7.15-2.8-9.88-.1-2.73 2.71-2.73 7.08 0 9.79s7.15 2.71 9.88 0C18.32 15.65 19 14.08 19 12.1h2c0 1.98-.88 4.55-2.64 6.29-3.51 3.48-9.21 3.48-12.72 0-3.5-3.47-3.53-9.11-.02-12.58s9.14-3.47 12.65 0L21 3v7.12z" fill="#335EEA"/>
+                          <path d="M12.5 8v4.25l3.5 2.08-.72 1.21L11 13V8h1.5z" fill="#335EEA" opacity="0.3"/>
+                        </svg>
+                      </div>
+                      <div class="ml-4">
+                        <h6 class="font-weight-bold text-uppercase text-primary mb-0">AI 赋能服务</h6>
+                        <p class="font-size-sm text-gray-700 mb-0">AI培训与Agent落地</p>
+                      </div>
+                    </a>
+                  </div>
                 </div>
-
                 <div class="col-6 col-lg-6">
-                  <h5 class="dropdown-header mb-5">咨询服务</h5>
-                  <a class="dropdown-item mb-3" href="case-devops.html">数字化咨询服务</a>
-                  <a class="dropdown-item mb-3" href="case-tech.html">系统开发设计</a>
-                  <a class="dropdown-item mb-3" href="case-tech.html">用户体验设计</a>
-                  <a class="dropdown-item mb-3" href="case-tech.html">AI应用开发</a>
+                  <h6 class="dropdown-header text-uppercase text-primary">方案</h6>
+                  <div class="list-group list-group-flush">
+                    <a class="list-group-item" href="#">
+                      <div class="icon icon-sm text-primary">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 3c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm7 13H5v-.23c0-.62.28-1.2.76-1.58C7.47 15.82 9.64 15 12 15s4.53.82 6.24 2.19c.48.38.76.97.76 1.58V19z" fill="#335EEA"/>
+                        </svg>
+                      </div>
+                      <div class="ml-4">
+                        <h6 class="font-weight-bold text-uppercase text-primary mb-0">医疗健康</h6>
+                        <p class="font-size-sm text-gray-700 mb-0">健康管理与数字疗法</p>
+                      </div>
+                    </a>
+                    <a class="list-group-item" href="#">
+                      <div class="icon icon-sm text-primary">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" fill="#335EEA"/>
+                        </svg>
+                      </div>
+                      <div class="ml-4">
+                        <h6 class="font-weight-bold text-uppercase text-primary mb-0">智能制造</h6>
+                        <p class="font-size-sm text-gray-700 mb-0">生产数字化转型</p>
+                      </div>
+                    </a>
+                    <a class="list-group-item" href="#">
+                      <div class="icon icon-sm text-primary">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" fill="#335EEA"/>
+                        </svg>
+                      </div>
+                      <div class="ml-4">
+                        <h6 class="font-weight-bold text-uppercase text-primary mb-0">注意力培训</h6>
+                        <p class="font-size-sm text-gray-700 mb-0">青少年认知能力训练</p>
+                      </div>
+                    </a>
+                  </div>
                 </div>
-
               </div>
             </div>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarSolution" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">产品系列</a>
-            
-            <div class="dropdown-menu dropdown-menu-md" aria-labelledby="navbarSolution">
+            <a class="nav-link dropdown-toggle" id="navbarProducts" data-toggle="dropdown" href="#" aria-haspopup="true" aria-expanded="false">产品系列</a>
+            <div class="dropdown-menu dropdown-menu-md" aria-labelledby="navbarProducts">
               <div class="list-group list-group-flush">
-
-                 <a class="list-group-item" href="solution-digitalloom.html">
+                <a class="list-group-item" href="solution-digitalloom.html">
                   <div class="icon icon-sm text-primary">
                     <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
                       <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -63,54 +122,17 @@
                   </div>
                   <span class="badge badge-pill badge-primary-soft ml-auto">1.0.0</span>
                 </a>
-
-                <a class="list-group-item" href="solution1.html">
-                  <div class="icon icon-sm text-primary">
-                    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <rect x="0" y="0" width="24" height="24"></rect>
-                        <path d="M8,3 L8,3.5 C8,4.32842712 8.67157288,5 9.5,5 L14.5,5 C15.3284271,5 16,4.32842712 16,3.5 L16,3 L18,3 C19.1045695,3 20,3.8954305 20,5 L20,21 C20,22.1045695 19.1045695,23 18,23 L6,23 C4.8954305,23 4,22.1045695 4,21 L4,5 C4,3.8954305 4.8954305,3 6,3 L8,3 Z" fill="#335EEA" opacity="0.3"></path>
-                        <path d="M11,2 C11,1.44771525 11.4477153,1 12,1 C12.5522847,1 13,1.44771525 13,2 L14.5,2 C14.7761424,2 15,2.22385763 15,2.5 L15,3.5 C15,3.77614237 14.7761424,4 14.5,4 L9.5,4 C9.22385763,4 9,3.77614237 9,3.5 L9,2.5 C9,2.22385763 9.22385763,2 9.5,2 L11,2 Z" fill="#335EEA"></path>
-                        <rect fill="#335EEA" opacity="0.3" x="7" y="10" width="5" height="2" rx="1"></rect>
-                        <rect fill="#335EEA" opacity="0.3" x="7" y="14" width="9" height="2" rx="1"></rect>
-                      </g>
-                    </svg>
-                  </div>
-                  <div class="ml-4">
-                    <h6 class="font-weight-bold text-uppercase text-primary mb-0">TempoSnap时影</h6>
-                    <p class="font-size-sm text-gray-700 mb-0">捕捉光影的瞬间，重塑你的万象。</p>
-                  </div>
-                </a>
-
-                <a class="list-group-item" href="product-tempoflow.html">
-                  <div class="icon icon-sm text-primary">
-                    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                        <rect x="0" y="0" width="24" height="24"></rect>
-                        <rect fill="#335EEA" x="4" y="4" width="7" height="7" rx="1.5"></rect>
-                        <path d="M5.5,13 L9.5,13 C10.3284271,13 11,13.6715729 11,14.5 L11,18.5 C11,19.3284271 10.3284271,20 9.5,20 L5.5,20 C4.67157288,20 4,19.3284271 4,18.5 L4,14.5 C4,13.6715729 4.67157288,13 5.5,13 Z M14.5,4 L18.5,4 C19.3284271,4 20,4.67157288 20,5.5 L20,9.5 C20,10.3284271 19.3284271,11 18.5,11 L14.5,11 C13.6715729,11 13,10.3284271 13,9.5 L13,5.5 C13,4.67157288 13.6715729,4 14.5,4 Z M14.5,13 L18.5,13 C19.3284271,13 20,13.6715729 20,14.5 L20,18.5 C20,19.3284271 19.3284271,20 18.5,20 L14.5,20 C13.6715729,20 13,19.3284271 13,18.5 L13,14.5 C13,13.6715729 13.6715729,13 14.5,13 Z" fill="#335EEA" opacity="0.3"></path>
-                      </g>
-                    </svg>
-                  </div>
-                  <div class="ml-4">
-                    <h6 class="font-weight-bold text-uppercase text-primary mb-0">TempoFlow节律</h6>
-                    <p class="font-size-sm text-gray-700 mb-0">听懂身体的节奏，找回你的心流。</p>
-                  </div>
-                </a>
-
-               
-                
               </div>
             </div>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarnews" href="news.html">客户案例</a>
+          <li class="nav-item">
+            <a class="nav-link" href="news.html">客户案例</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="about.html">关于我们</a>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">关于我们</a>
           </li>
         </ul>
-        <a class="navbar-btn btn btn-sm btn-primary lift btn-sm ml-auto" href="" target="_blank">登录</a>
+        <a class="navbar-btn btn btn-sm btn-primary lift ml-auto" href="contact.html">联系我们</a>
       </div>
     </div>
   `;
@@ -150,39 +172,32 @@
           </p>
         </div>
         <div class="col-6 col-md-4 col-lg-2">
-          <h6 class="font-weight-bold text-uppercase text-gray-700">产品和服务</h6>
+          <h6 class="font-weight-bold text-uppercase text-gray-700">服务</h6>
           <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
-            <li class="mb-3"><a href="case-business.html" class="text-reset">业务中台</a></li>
-            <li class="mb-3"><a href="case-data.html" class="text-reset">数据中台</a></li>
-            <li class="mb-3"><a href="case-ai.html" class="text-reset">AI中台</a></li>
-            <li class="mb-3"><a href="case-devops.html" class="text-reset">研发效能平台</a></li>
-            <li class="mb-3"><a href="case-tech.html" class="text-reset">基础技术平台</a></li>
+            <li class="mb-3"><a href="#" class="text-reset">软件系统开发</a></li>
+            <li class="mb-3"><a href="#" class="text-reset">产品体验设计</a></li>
+            <li class="mb-3"><a href="#" class="text-reset">AI 赋能服务</a></li>
           </ul>
         </div>
         <div class="col-6 col-md-4 col-lg-2">
-          <h6 class="font-weight-bold text-uppercase text-gray-700">典型案例</h6>
+          <h6 class="font-weight-bold text-uppercase text-gray-700">方案</h6>
           <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
-            <li class="mb-3"><a href="solution1.html" class="text-reset">零售解决方案</a></li>
-            <li class="mb-3"><a href="solution2.html" class="text-reset">珠宝分销行业方案</a></li>
-            <li class="mb-3"><a href="solution3.html" class="text-reset">智能制造方案</a></li>
+            <li class="mb-3"><a href="#" class="text-reset">医疗健康</a></li>
+            <li class="mb-3"><a href="#" class="text-reset">智能制造</a></li>
+            <li class="mb-3"><a href="#" class="text-reset">注意力培训</a></li>
           </ul>
         </div>
         <div class="col-6 col-md-4 offset-md-4 col-lg-2 offset-lg-0">
-          <h6 class="font-weight-bold text-uppercase text-gray-700">帮助与支持</h6>
+          <h6 class="font-weight-bold text-uppercase text-gray-700">产品与案例</h6>
           <ul class="list-unstyled text-muted mb-0">
-            <li class="mb-3"><a target="_blank" href="contact.html" class="text-reset">合作咨询</a></li>
-            <li class="mb-3"><a target="_blank" href="tech-doc.html" class="text-reset">技术文档</a></li>
-            <li class="mb-3"><a href="news.html" class="text-reset">新闻资讯</a></li>
-            <li class="mb-3"><a href="log.html" class="text-reset">更新日志</a></li>
-            <li class="mb-3"><a href="#" class="text-reset">文档下载</a></li>
+            <li class="mb-3"><a href="solution-digitalloom.html" class="text-reset">数智织机</a></li>
+            <li class="mb-3"><a href="news.html" class="text-reset">客户案例</a></li>
           </ul>
         </div>
         <div class="col-6 col-md-4 col-lg-2">
           <h6 class="font-weight-bold text-uppercase text-gray-700">关于我们</h6>
           <ul class="list-unstyled text-muted mb-0">
-            <li class="mb-3"><a href="about.html" class="text-reset">关于我们</a></li>
-            <li class="mb-3"><a href="about.html" class="text-reset">企业文化</a></li>
-            <li class="mb-3"><a href="about.html" class="text-reset">加入我们</a></li>
+            <li class="mb-3"><a href="about.html" class="text-reset">公司介绍</a></li>
             <li class="mb-3"><a href="contact.html" class="text-reset">联系我们</a></li>
           </ul>
         </div>
